440fa32665e201697ac28445db6921fb5c1c038a
docktermj <michael@dockter.com>
2023-08-24 16:59:13 -0400

    #99 Add quotes

--- g2configmgr/g2configmgr.go
+++ g2configmgr/g2configmgr.go
@@ -6,9 +6,9 @@ package g2configmgr
 /*
 #include "g2configmgr.h"
 #cgo CFLAGS: -g -I/opt/senzing/g2/sdk/c
-#cgo windows CFLAGS: -g -IC:/Program Files/senzing/g2/sdk/c
+#cgo windows CFLAGS: -g -I"C:/Program Files/senzing/g2/sdk/c"
 #cgo LDFLAGS: -L/opt/senzing/g2/lib -lG2
-#cgo windows LDFLAGS: -LC:/Program Files/senzing/g2/lib -lG2
+#cgo windows LDFLAGS: -L"C:/Program Files/senzing/g2/lib" -lG2
 */
 import "C"
 
