f2dd0f384f1909b0846cae5f7ab49e77eda1fec0
docktermj <michael@dockter.com>
2023-08-25 10:00:47 -0400

    #99 Use new GitHub action to install Windows - 1

--- g2config/g2config.go
+++ g2config/g2config.go
@@ -6,9 +6,9 @@ package g2config
 /*
 #include "g2config.h"
 #cgo CFLAGS: -g -I/opt/senzing/g2/sdk/c
-#cgo windows CFLAGS: -g -I"C:/Program Files/senzing/g2/sdk/c"
+#cgo windows CFLAGS: -g -I"C:/Program Files/Senzing/g2/sdk/c"
 #cgo LDFLAGS: -L/opt/senzing/g2/lib -lG2
-#cgo windows LDFLAGS: -L"C:/Program Files/senzing/g2/lib" -lG2
+#cgo windows LDFLAGS: -L"C:/Program Files/Senzing/g2/lib" -lG2
 */
 import "C"
 
