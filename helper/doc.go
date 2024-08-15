93c00d8572b99769e3ed093a800fd20fb3cfe1d0
Michael Dockter <michael@dockter.com>
2024-08-15 12:07:21 -0400

    #156 helper draft

---
+++ helper/doc.go
@@ -0,0 +1,12 @@
+/*
+The helper package is not intended for public use.
+It contains functions that are common to the
+[szconfig], [szconfigmanager], [szdiagnostic], [szengine], and  [szproduct] packages.
+
+[szconfig]: https://pkg.go.dev/github.com/senzing-garage/sz-sdk-go-core/szconfig
+[szconfigmanager]: https://pkg.go.dev/github.com/senzing-garage/sz-sdk-go-core/szconfigmanager
+[szdiagnostic]: https://pkg.go.dev/github.com/senzing-garage/sz-sdk-go-core/szdiagnostic
+[szengine]: https://pkg.go.dev/github.com/senzing-garage/sz-sdk-go-core/szengine
+[szproduct]: https://pkg.go.dev/github.com/senzing-garage/sz-sdk-go-core/szproduct
+*/
+package helper
