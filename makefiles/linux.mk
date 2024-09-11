0408285a2ff07f3f569a513da1e6a19c2cb7c402
docktermj <michael@dockter.com>
2024-09-11 11:35:38 -0400

    #232 Prepare for versioned release

--- makefiles/linux.mk
+++ makefiles/linux.mk
@@ -64,8 +64,6 @@ setup-osarch-specific:
 .PHONY: test-osarch-specific
 test-osarch-specific:
 	@go test -json -v -p 1 ./... 2>&1 | tee /tmp/gotest.log | gotestfmt
-	# @go test -json -v -p 1 ./szengine 2>&1 | tee /tmp/gotest.log | gotestfmt
-	# @go test -json -v -p 1 ./szengine -run TestSzengine_AddRecord_nilDataSourceCode 2>&1 | tee /tmp/gotest.log | gotestfmt
 
 # -----------------------------------------------------------------------------
 # Makefile targets supported only by this platform.
