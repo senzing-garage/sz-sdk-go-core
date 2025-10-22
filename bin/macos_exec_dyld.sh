45f1ad8c7295645277b37d068ea9529356b3d497
Michael Dockter <michael@dockter.com>
2025-10-22 18:00:11 -0400

    #342 Savepoint

--- bin/macos_exec_dyld.sh
+++ bin/macos_exec_dyld.sh
@@ -3,5 +3,8 @@
 export SENZING_PATH=${HOME}/senzing
 export DYLD_LIBRARY_PATH=${SENZING_PATH}/er/lib:${SENZING_PATH}/er/lib/macos
 export LD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}
+export CGO_CFLAGS="-g -I${SENZING_PATH}/er/sdk/c"
+export CGO_LDFLAGS="-L${SENZING_PATH}/er/lib -lSz -Wl,-no_warn_duplicate_libraries"
+export SENZING_TOOLS_DATABASE_URL="sqlite3://na:na@nowhere/tmp/sqlite/G2C.db"
 
 "$@"
